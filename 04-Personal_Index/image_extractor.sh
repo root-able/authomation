f9aa5d92172d6c88d62e43688af5140f88ad7d12
Paul Abdola <paul.trollux@gmail.com>
2019-12-27 11:45:54 +0100

    Fixing user inputs

--- 04-Personal_Index/image_extractor.sh
+++ 04-Personal_Index/image_extractor.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-SOURCE_FOLDER_LIST="/volume1/Cloudstation/Images/"
-DEST_FOLDER="/volume1/Cloudstation/Images - Index/Images - Index - Flat/"
+SOURCE_FOLDER_LIST="$1"
+DEST_FOLDER="$2"
+USER="$3"
+GROUP="$4"
+
 EXT_WHITELIST='\.(jpg|jpeg|gif|png|tif|webp)$'
 
 OLDIFS=$IFS
@@ -20,7 +23,7 @@ do
 done
 
 DEST_PERM="700"
-DEST_OWNER="Paul-Administrator:users"
+DEST_OWNER="$USER:$GROUP"
 
 chmod -R "$DEST_PERM" "$DEST_FOLDER"
 chown -R "$DEST_OWNER" "$DEST_FOLDER"
--- 04-Personal_Index/image_sorter.sh
+++ 04-Personal_Index/image_sorter.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-SOURCE_FOLDER="/volume1/Cloudstation/Images - Index/Images - Index - Flat/"
-DEST_FOLDER="/volume1/Cloudstation/Images - Index/Images - Index - Chrono/"
+SOURCE_FOLDER="$1"
+DEST_FOLDER="$2"
+USER="$3"
+GROUP="$4"
 EXT_WHITELIST='\.(jpg|jpeg|gif|png|tif|webp)$'
 PATH_PATTERN='+%Y/%m - %B/%d - %A'
 
@@ -30,7 +32,7 @@ do
 done
 
 DEST_PERM="700"
-DEST_OWNER="Paul-Administrator:users"
+DEST_OWNER="$USER:GROUP"
 
 chmod -R "$DEST_PERM" "$DEST_FOLDER"
 chown -R "$DEST_OWNER" "$DEST_FOLDER"
