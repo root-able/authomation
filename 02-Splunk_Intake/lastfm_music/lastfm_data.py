4a43f9209b319e18f737872d46edb6dab2499361
root <root@pipaul>
2020-02-06 22:44:09 +0100

    Added logging + fixed bug on double track with last_epoch

--- 02-Splunk_Intake/lastfm_music/lastfm_data.py
+++ 02-Splunk_Intake/lastfm_music/lastfm_data.py
@@ -4,11 +4,12 @@
 import lastexport
 import os
 from optparse import OptionParser
-import splunklib.client as client
-import splunklib.client
+import syslog
 import sys
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+syslog.openlog("lastfm_data.py")
 
 if len(sys.argv) != 2:
     sys.exit("first arg as user")
@@ -32,19 +33,23 @@ with open("exported_tracks.txt", "r") as f:
         for l in f.readlines():
             lst = l.split("\t")
             # New last epoch
-            if lst[0] < last_epoch:
+            if lst[0] <= last_epoch:
                 break
             else:
                 # key=value format
                 final.append("date=\"{}\" track=\"{}\" artist=\"{}\" album=\"{}\"\n".format(lst[0], lst[1], lst[2], lst[3]))
             if first: # The first line is the last song played
-                with open("last_epoch", "w") as le_new:
+                with open(os.path.join(__location__ , "last_epoch"), "w") as le_new:
                     le_new.write(lst[0])
+                    syslog.syslog("new last_epoch={}".format(lst[0]))
                     first = False
 
 os.remove("exported_tracks.txt") 
+
+final.reverse()
 
 # TODO - Trouver un meilleur endroit
 with open("/var/log/last-fm_tracks.log", "a+") as log:
     for track in final:
         log.write(track)
+    syslog.syslog("Wrote {} new songs".format(len(final)))
